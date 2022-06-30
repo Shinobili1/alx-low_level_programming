7915e6c27622824d5fb23cda2a023dc4d3e6f6b5
Biliyoung Aishino <biliyounga@gmail.com>
2022-06-30 14:56:56 -0700

    C program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
 *main prints size of various types
 *Return 0: if exited properly
 */
-int main (void)
+int main(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
