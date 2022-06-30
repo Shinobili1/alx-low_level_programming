f30b4a27f54cc38fe970eef831490d8d41297903
Biliyoung Aishino <biliyounga@gmail.com>
2022-06-30 14:25:00 -0700

    C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
+#include <studio.h>
 
 int main(void)
-
 {
-puts("\"programming is like building a multilingual puzzle\n");
+puts("\"Programming is like a multilingual puzzle \n");
 return (0);
 }
-
