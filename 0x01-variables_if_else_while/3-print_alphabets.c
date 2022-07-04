a6a2ab757a69dfa077de644710e2dee3c21d2a42
Biliyoung Aishino <biliyounga@gmail.com>
2022-07-04 14:25:11 -0700

    alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,15 @@ int main(void)
 {
 int l;
 
-for (l = a; l <= z; l++)
+for (l = 97; l <= 122; l++)
+{
 putchar(l);
-
-for (l = A; l <= Z; l++)
+}
+for (l = 65; l <= 90; l++)
+{
 putchar(l);
-
-putchar(0);
+}
+putchar(10);
 
 return (0);
 }
