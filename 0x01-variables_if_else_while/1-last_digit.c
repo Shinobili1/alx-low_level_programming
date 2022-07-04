08f27d384d06dea51453c23cba8688db5279fa9a
Biliyoung Aishino <biliyounga@gmail.com>
2022-07-04 11:56:15 -0700

    last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is %d and is ", n);
+printf("Last digit of %d is and is ", n);
 if (n > 5)
 {
 printf(" greater than 5");
