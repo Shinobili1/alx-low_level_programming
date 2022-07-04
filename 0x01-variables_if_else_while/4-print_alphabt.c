20d329caf145a9e9d3c7a40f804ae6e1b8879728
Biliyoung Aishino <biliyounga@gmail.com>
2022-07-04 14:33:21 -0700

    prnt alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char l;
+
+for (l = a; letter <= z; letter++)
+{
+if (l != e && letter != q)
+{
+putchar(l);
+}
+}
+putchar(10);
+
+return (0);
+}
