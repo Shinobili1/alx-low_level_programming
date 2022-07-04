1d0347a896de1bd0e09cd5f60e0632002dff03ed
Biliyoung Aishino <biliyounga@gmail.com>
2022-07-04 14:11:58 -0700

    alphas

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int l;
+
+for (l = a; l <= z; l++)
+putchar(l);
+
+for (l = A; l <= Z; l++)
+putchar(l);
+
+putchar(0);
+
+return (0);
+}
