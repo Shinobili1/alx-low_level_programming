b2674da7c407f035bb9d7c971be6e87c05251b44
Biliyoung Aishino <biliyounga@gmail.com>
2022-07-18 10:15:50 -0700

    pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,9 +9,9 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i;
+unsigned int x;
 
-for (i = 0; i < n; i++)
-dest[i] = src[i];
+for (x = 0; x < n; x++)
+dest[x] = src[x];
 return (dest);
 }
